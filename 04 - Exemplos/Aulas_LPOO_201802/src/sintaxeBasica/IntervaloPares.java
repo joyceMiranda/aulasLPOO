a866377561fe6fd356056217174996638ea59058
Joyce Miranda <mds.joyce@gmail.com>
2018-08-06 16:34:01 -0400

    ...

---
+++ 04 - Exemplos/Aulas_LPOO_201802/src/sintaxeBasica/IntervaloPares.java
@@ -0,0 +1,19 @@
+package sintaxeBasica;
+
+public class IntervaloPares {
+
+    public static void main(String[] args) {
+        int x=15, y=30;
+        if(x >= y){
+            System.out.println("Erro! Informe x < y");
+        }else{
+            System.out.println("Valores pares do Intervalo:");
+            for(int i=x; i < y; i++){
+                if( i%2 == 0 ){
+                    System.out.print(" " + i);
+                }
+            }
+        }
+    }
+    
+}
---
+++ 04 - Exemplos/Aulas_LPOO_201802/src/sintaxeBasica/MediaPonderada.java
@@ -0,0 +1,21 @@
+package sintaxeBasica;
+
+import java.text.DecimalFormat;
+
+public class MediaPonderada {
+    
+    public static void main(String[] args) {
+        
+        double n1=6.0, n2=5.0, n3=3.0, media; 
+        int p1=1, p2=2, p3=3;
+        String aluno = "João";
+        
+        media = (n1*p1 + n2*p2 + n3*p3)/(p1+p2+p3);
+        
+        DecimalFormat df = new DecimalFormat(".00");
+        System.out.println("A média de " + 
+                           aluno + " foi " + df.format(media));
+        
+    }
+    
+}
